9f0a026b9315ecd9ae8c318c4e45f984d15fbb39
Dmitriy <dmitriy.fokeev.92@mail.ru>
2020-05-01 22:05:24 +0300

    Ferst homework

--- free/free.js
+++ free/free.js
@@ -1,1 +1,26 @@
-console.log('dfgd');
+let money = +prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+let quesExp = +prompt("Введите обязательную статью расходов в этом месяце", "");
+let quesExp2 = +prompt("Во сколько обойдется?", "");
+
+
+
+
+let appData = {
+	"budjet" : money,
+	"timeData" : time,
+	"expenses" : {},
+	"optionalExpenses" : {},
+	"income" : [],
+	"saving" : false,
+};
+
+appData.expenses.quesExp = quesExp2;
+
+alert(appData.budjet / 30);
+
+console.log(appData.expenses.quesExp);
+
+
+console.log(appData.budjet);
